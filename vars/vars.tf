962575411d4c310e83928cf1d641d263fdd50b46
Harsha1Devops <166914555+Harsha1Devops@users.noreply.github.com>
2025-02-13 15:50:52 +0530

    Vars

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,3 +1,11 @@
-variable "sample" {
-    default = "welcome to terraform"
+variable "example_op" {
+    default = {
+        batch = "class" 
+        traning = "AWs with Devops"
+        Name = "Batch 55"
+    }
+}
+
+output "example_ops" {
+    value = "we are learning ${var.example_ops[batch] and we are trying ${var.example_ops[traning]} and it is ${var.example_ops[Name]}"
 }
